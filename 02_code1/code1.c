e84d03d3893795883b44bdff831555429b90a857
Learn 2 Program Grader <invalid@nowhere.com>
2022-11-01 05:40:11 +0000

    Released assignment

---
+++ 02_code1/code1.c
@@ -0,0 +1,16 @@
+int max (int num1, int num2) {
+  //check if num1 is greater than num2
+    //if so, your answer is num1
+    //otherwise, your answer is num2
+}
+
+int main(void) {
+  printf("max(42, -69) is %d\n", max(42, -69));
+  printf("max(33, 0) is %d\n", max(33, 0));
+  printf("max(0x123456, 123456) is %d\n", max(0x123456, 123456));
+  //compute the max of 0x451215AF and 0x913591AF and print it out as a decimal number
+  
+  return 0;
+}
+
+
