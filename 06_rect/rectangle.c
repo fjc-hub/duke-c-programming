8b95839c3bfed9f45b55611a44e39626820ec626
fjc-hub <1661007260@qq.com>
2022-11-06 17:03:31 +0800

    complete assignment 06

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,14 +16,36 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+typedef struct {
+  int x, y, width, height;
+} rectangle;
 
 rectangle canonicalize(rectangle r) {
-  //WRITE THIS FUNCTION
+  if (r.width < 0) {
+    r.x += r.width;
+    r.width = -r.width;
+  }
+  if (r.height < 0) {
+    r.y += r.height;
+    r.height = -r.height;
+  }
   return r;
 }
+
 rectangle intersection(rectangle r1, rectangle r2) {
-  //WRITE THIS FUNCTION
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  int x = min(r1.x + r1.width, r2.x + r2.width);
+  int y = min(r1.y + r1.height, r2.y + r2.height);
+  r1.x = max(r1.x, r2.x);
+  r1.y = max(r1.y, r2.y);
+  if (r1.x > x || r1.y > y) {
+    r1.width = 0;
+    r1.height = 0;
+    return r1;
+  }
+  r1.width = x - r1.x;
+  r1.height = y - r1.y;
   return r1;
 }
 
