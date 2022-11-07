7aad7e305ca9b7f83839b7afb09fce2fc4c2a0e4
Learn 2 Program Grader <invalid@nowhere.com>
2022-11-07 09:34:08 +0000

    Released assignment

---
+++ c2prj1_cards/cards.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "cards.h"
+
+
+void assert_card_valid(card_t c) {
+
+
+}
+
+const char * ranking_to_string(hand_ranking_t r) {
+  return "";
+}
+
+char value_letter(card_t c) {
+  return 'x';
+}
+
+
+char suit_letter(card_t c) {
+  return 'x';
+  
+}
+
+void print_card(card_t c) {
+
+}
+
+card_t card_from_letters(char value_let, char suit_let) {
+  card_t temp;
+  return temp;
+}
+
+card_t card_from_num(unsigned c) {
+  card_t temp;
+  return temp;
+}
