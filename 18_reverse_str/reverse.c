080e59c3a7a3ec78907be36422ae37fe08e7d086
Learn 2 Program Grader <invalid@nowhere.com>
2022-11-16 07:01:02 +0000

    Released assignment

---
+++ 18_reverse_str/reverse.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char * str) {
+  //WRITE ME!
+}
+
+int main(void) {
+  char str0[] = "";
+  char str1[] = "123";
+  char str2[] = "abcd";
+  char str3[] = "Captain's log, Stardate 42523.7";
+  char str4[] = "Hello, my name is Inigo Montoya.";
+  char str5[] = "You can be my wingman anyday!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char * array[] = {str0, str1, str2, str3, str4, str5, str6};
+  for (int i = 0; i < 7; i++) {
+    reverse(array[i]);
+    printf("%s\n", array[i]);
+  }
+  return EXIT_SUCCESS;
+}
