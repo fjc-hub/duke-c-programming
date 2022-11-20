60132782cf30df8f661c209444526c4decd06f42
Learn 2 Program Grader <invalid@nowhere.com>
2022-11-20 13:14:53 +0000

    Released assignment

---
+++ c3prj1_deck/deck.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "deck.h"
+void print_hand(deck_t * hand){
+
+}
+
+int deck_contains(deck_t * d, card_t c) {
+  return 0;
+}
+
+void shuffle(deck_t * d){
+
+}
+
+void assert_full_deck(deck_t * d) {
+
+}
