8cbc5c262674463c10912b9f9076c9e0e5e0ee62
Coursera Learner <nobody@nowhere.nul>
2022-11-27 10:57:43 +0000

    ?

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -2,7 +2,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include "rotate.h"
+
+void rotate(char matrix[10][10]) {
+    for (int i=0; i < 10; i++) {
+        for (int j=0; j < i; j++) {
+            int tmp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = tmp;
+        }
+    }
+    for (int i=0; i < 10; i++) {
+        for (int j=0; j < 5; j++) {
+            int tmp = matrix[i][j];
+            matrix[i][j] = matrix[i][9-j];
+            matrix[i][9-j] = tmp;
+        }
+    }
+    return;
+}
+
+
 
 int main(int argc, char ** argv) {
     if (argc != 2) {
