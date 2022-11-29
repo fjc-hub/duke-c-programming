e34c52323944a09cdaa2ea7749fbe2af82f4136e
Learn 2 Program Grader <invalid@nowhere.com>
2022-11-29 15:15:04 +0000

    Released assignment

---
+++ 30_sort_lines/sortLines.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//This function is used to figure out the ordering
+//of the strings in qsort.  You do not need
+//to modify it.
+int stringOrder(const void * vp1, const void * vp2) {
+  const char * const * p1 = vp1;
+  const char * const * p2 = vp2;
+  return strcmp(*p1, *p2);
+}
+//This function will sort and print data (whose length is count).
+void sortData(char ** data, size_t count) {
+  qsort(data, count, sizeof(char *), stringOrder);
+}
+
+int main(int argc, char ** argv) {
+  
+  //WRITE YOUR CODE HERE!
+  
+  return EXIT_SUCCESS;
+}
