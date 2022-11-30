c102138fa90d57fd71c75f1660d2e329ba2c5208
Learn 2 Program Grader <invalid@nowhere.com>
2022-11-30 08:33:54 +0000

    Released assignment

---
+++ 32_kvs/kv.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "kv.h"
+
+
+
+kvarray_t * readKVs(const char * fname) {
+  //WRITE ME
+}
+
+void freeKVs(kvarray_t * pairs) {
+  //WRITE ME
+}
+
+void printKVs(kvarray_t * pairs) {
+  //WRITE ME
+}
+
+char * lookupValue(kvarray_t * pairs, const char * key) {
+  //WRITE ME
+}
