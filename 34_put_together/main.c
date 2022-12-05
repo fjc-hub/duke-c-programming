4fde56cda4231ec9e5443c398f25b8471b252b38
Learn 2 Program Grader <invalid@nowhere.com>
2022-12-05 11:18:31 +0000

    Released assignment

---
+++ 34_put_together/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "kv.h"
+#include "counts.h"
+#include "outname.h"
+
+counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
+  //WRITE ME
+  return NULL;
+}
+
+int main(int argc, char ** argv) {
+  //WRITE ME (plus add appropriate error checking!)
+ //read the key/value pairs from the file named by argv[1] (call the result kv)
+
+ //count from 2 to argc (call the number you count i)
+
+    //count the values that appear in the file named by argv[i], using kv as the key/value pair
+    //   (call this result c)
+
+    //compute the output file name from argv[i] (call this outName)
+
+
+    //open the file named by outName (call that f)
+
+    //print the counts from c into the FILE f
+
+    //close f
+
+    //free the memory for outName and c
+
+
+
+ //free the memory for kv
+
+  return EXIT_SUCCESS;
+}
