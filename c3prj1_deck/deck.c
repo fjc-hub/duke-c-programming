ac60580c3ee742c3c2db455169c30b2a88d2e25e
fjc-hub <morganfu@tencent.com>
2022-12-06 15:15:45 +0800

    fix

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -54,8 +54,11 @@ void assert_full_deck(deck_t * d) {
 }
 
 void add_card_to(deck_t * deck, card_t c) {
+  card_t *card = malloc(sizeof(*card));
+  card->value = c.value;
+  card->suit = c.suit;
   deck->cards = realloc(deck->cards, (deck->n_cards + 1)*sizeof(*(deck->cards)));
-  deck->cards[deck->n_cards] = &c;
+  deck->cards[deck->n_cards] = &card;
   deck->n_cards++;
 }
 
