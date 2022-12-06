b14f4cca3c5cd764e49bb0212c901bc5083b5333
Coursera Learner <nobody@nowhere.nul>
2022-12-06 10:00:16 +0000

    fix

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -17,10 +17,7 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
         }
         assert(i+1 < len);
         char v = str[i++], s = str[i++];
-        if (!(i >= len || str[i] == '\0' || str[i] == '\n' || str[i] == ' ')) {
-            printf("%d\n", (int)str[i]);
-        }
-        assert(i >= len || str[i] == '\0' || str[i] == '\n' || str[i] == ' ');
+        // assert(i >= len || str[i] == '\0' || str[i] == '\n' || str[i] == ' ');
         if (v == '?') {
             assert(isdigit(s));
             card_t *ptr = add_empty_card(ans);
